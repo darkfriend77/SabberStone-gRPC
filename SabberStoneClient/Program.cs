21533c28635d1e1ad3e94eb015ff5854635cf89f
darkfriend77 <darkfriend@swissonline.ch>
2019-05-15 22:02:08 +0200

    initial commit

---
+++ SabberStoneClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Grpc.Core;
+
+namespace SabberStoneClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var channel = new Channel("127.0.0.1:50051", ChannelCredentials.Insecure);
+
+            var client = new TestgRPC.TestgRPCClient(channel);
+            var user = "me";
+
+            var reply = client.SayHello(new HelloRequest { Name = user });
+            Console.WriteLine($"Greeting: {reply.Message}");
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SabberStoneServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace SabberStoneServer
+{
+    internal class TestgRpcImpl : TestgRPC.TestgRPCBase
+    {
+        // Server side handler of the SayHello RPC
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            Console.WriteLine(context.Peer);
+
+            var reply = new HelloReply {Message = $"Hello {request.Name}"};
+
+            return Task.FromResult(reply);
+        }
+    }
+
+    public class Program
+    {
+        private const int Port = 50051;
+
+        public static void Main(string[] args)
+        {
+            var server = new Server
+            {
+                Services = { TestgRPC.BindService(new TestgRpcImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine($"Greeter server listening on port {Port}");
+            Console.WriteLine("Press any key to stop the server...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
+        }
+    }
+}
