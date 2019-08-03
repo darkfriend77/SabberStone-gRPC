4bb703d078525c69b515a1cfc4e633367616a497
darkfriend77 <darkfriend@swissonline.ch>
2019-08-03 12:21:22 +0200

    added CreateMatchGameReply

--- SabberStoneServer/Services/ReflectEntities.cs
+++ SabberStoneServer/Services/ReflectEntities.cs
@@ -2,7 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Google.Protobuf.Collections;
 using SabberStoneContract.Model;
+using SabberStoneCore.Model.Entities;
+using SabberStoneCore.Model.Zones;
+
+using static Controller;
 
 namespace SabberStoneServer.Services
 {
@@ -10,9 +15,155 @@ namespace SabberStoneServer.Services
     {
         private MatchGameReply CreateMatchGameReply()
         {
-            MatchGameReply result = new MatchGameReply();
+            MatchGameReply result = new MatchGameReply()
+            {
+                GameId = GameId,
+                CurrentPlayer = GetController(_game.CurrentPlayer),
+                CurrentOpponent = GetController(_game.CurrentOpponent),
+                State = (MatchGameReply.Types.State)_game.State,
+                Turn = _game.Turn
+        };
 
             return result;
         }
+
+        private Controller GetController(SabberStoneCore.Model.Entities.Controller controller)
+        {
+            return new Controller()
+            {
+                Id = controller.PlayerId,
+                Hero = GetHero(controller.Hero),
+                BoardZone = GetBoardZone(controller.BoardZone),
+                HandZone = GetHandZone(controller.HandZone),
+                SecretZone = GetSecretZone(controller.SecretZone),
+                DeckZone = GetDeckZone(controller.DeckZone),
+                PlayState = (Types.PlayState)controller.PlayState,
+                BaseMana = controller.BaseMana,
+                RemainingMana = controller.RemainingMana,
+                OverloadLocked = controller.OverloadLocked,
+                OverloadOwed = controller.OverloadOwed
+            };
+        }
+
+        private BoardZone GetBoardZone(SabberStoneCore.Model.Zones.BoardZone zone)
+        {
+            var result = new BoardZone();
+            var span = zone.GetSpan();
+            for (int i = 0; i < span.Length; i++)
+                result.Minions.Add(GetMinion(span[i]));
+            return result;
+        }
+
+        private HandZone GetHandZone(SabberStoneCore.Model.Zones.HandZone zone)
+        {
+            var result = new HandZone();
+            var span = zone.GetSpan();
+            for (int i = 0; i < span.Length; i++)
+                result.Entities.Add(GetPlayable(span[i], true));
+            return result;
+        }
+
+        private SecretZone GetSecretZone(SabberStoneCore.Model.Zones.SecretZone zone)
+        {
+            var result = new SecretZone();
+            var span = zone.GetSpan();
+            for (int i = 0; i < span.Length; i++)
+                result.Entities.Add(GetPlayable(span[i], true));
+            return result;
+        }
+
+        private DeckZone GetDeckZone(SabberStoneCore.Model.Zones.DeckZone zone)
+        {
+            var result = new DeckZone();
+            var span = zone.GetSpan();
+            for (int i = 0; i < span.Length; i++)
+                result.Entities.Add(GetPlayable(span[i], true));
+            return result;
+        }
+
+        private Playable GetPlayable(IPlayable playable, bool hand)
+        {
+            bool isCharacter = playable is Character;
+            Character c = isCharacter ? playable as Character : null;
+            return new Playable()
+            {
+                CardId = playable.Card.AssetId,
+                Cost = playable.Cost,
+                Atk = isCharacter ? c.AttackDamage : 0,
+                BaseHealth = isCharacter ? c.BaseHealth : 0,
+                Ghostly = hand ? playable[SabberStoneCore.Enums.GameTag.GHOSTLY] == 1 : false
+            };
+        }
+
+        private Minion GetMinion(SabberStoneCore.Model.Entities.Minion minion)
+        {
+            return new Minion()
+            {
+                CardId = minion.Card.AssetId,
+                Atk = minion.AttackDamage,
+                BaseHealth = minion.BaseHealth,
+                Damage = minion.Damage,
+                NumAttacksThisTurn = minion.NumAttacksThisTurn,
+                Exhausted = minion.IsExhausted,
+                AttackableByRush = minion.AttackableByRush,
+                Charge = minion.HasCharge,
+                Windfury = minion.HasWindfury,
+                Lifesteal = minion.HasLifeSteal,
+                Poisonous = minion.Poisonous,
+                Stealth = minion.HasStealth,
+                DivineShield = minion.HasDivineShield,
+                Immune = minion.IsImmune,
+                Elusive = minion.CantBeTargetedBySpells,
+                Frozen = minion.IsFrozen,
+                Deathrattle = minion.HasDeathrattle,
+
+                ZonePosition = minion.ZonePosition,
+                OrderOfPlay = minion.OrderOfPlay
+            };
+        }
+
+        private Hero GetHero(SabberStoneCore.Model.Entities.Hero hero)
+        {
+            return new Hero()
+            {
+                CardClass = (int)hero.Card.Class,
+                Atk = hero.AttackDamage,
+                BaseHealth = hero.BaseHealth,
+                Damage = hero.Damage,
+                NumAttacksThisTurn = hero.NumAttacksThisTurn,
+                Armor = hero.Armor,
+                Exhausted = hero.IsExhausted,
+                Stealth = hero.HasStealth,
+                Immune = hero.IsImmune,
+
+                Power = GetHeroPower(hero.HeroPower),
+                Weapon = hero.Weapon != null ? GetWeapon(hero.Weapon) : null
+            };
+        }
+
+        private HeroPower GetHeroPower(SabberStoneCore.Model.Entities.HeroPower heroPower)
+        {
+            return new HeroPower()
+            {
+                CardId = heroPower.Card.AssetId,
+                Cost = heroPower.Cost,
+                Exhausted = heroPower.IsExhausted
+            };
+        }
+
+        private Weapon GetWeapon(SabberStoneCore.Model.Entities.Weapon weapon)
+        {
+            return new Weapon()
+            {
+                CardId = weapon.Card.AssetId,
+                Atk = weapon.AttackDamage,
+                Durability = weapon.Durability,
+                Windfury = weapon.IsWindfury,
+                Lifesteal = weapon.HasLifeSteal,
+                Poisonous = weapon.Poisonous,
+                Immune = weapon.IsImmune,
+            };
+        }
+
     }
 }
